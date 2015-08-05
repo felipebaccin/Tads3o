98e0a2ff7fb07d8d5dd6b4a6c41cb1e908be105b
Felipe Baccin <felipebaccincvel@gmail.com>
2015-08-05 19:39:58 -0300

    teste de alteração de java doc

--- src/Qualquer.java
+++ src/Qualquer.java
@@ -1,8 +1,16 @@
-
+/**
+ * 
+ * classe que demostra o funcionamento do metodo main 
+ * 
+ * @author Felipe Baccin
+ *
+ */
 public class Qualquer {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		
+		
 	}
 	
 }
