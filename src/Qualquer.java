fcac1f3aa29380c8391a7781e3b5502a42ec1dab
Felipe Baccin <felipebaccincvel@gmail.com>
2015-08-05 19:45:58 -0300

    querido diario metodo criado para calcular

--- src/Qualquer.java
+++ src/Qualquer.java
@@ -10,7 +10,12 @@ public class Qualquer {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		
-		
+		int res = calc(2,2);
+		System.out.println(res);
+	}
+	
+	public static int calc(int a , int b){
+		return a+b;
 	}
 	
 }
